45ab3da814b09e22966365692a16988889bb14dd
Ryan Morrison <ryan.morrison04@gmail.com>
2012-09-21 13:03:08 -0400

    Handle exceptions where Steam IDs cannot be determined or the user's Steam ID is nonexistent

--- src/main/java/us/rddt/IRCBot/Handlers/SteamUserQuery.java
+++ src/main/java/us/rddt/IRCBot/Handlers/SteamUserQuery.java
@@ -28,9 +28,11 @@
 
 package us.rddt.IRCBot.Handlers;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.logging.Level;
 
+import org.json.JSONException;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.events.MessageEvent;
 
@@ -45,7 +47,7 @@ import us.rddt.IRCBot.Implementations.SteamUser;
 public class SteamUserQuery implements Runnable {
     // Variables
     private MessageEvent<PircBotX> event;
-    
+
     /**
      * Class constructor
      * @param event the MessageEvent that triggered this class
@@ -53,7 +55,7 @@ public class SteamUserQuery implements Runnable {
     public SteamUserQuery(MessageEvent<PircBotX> event) {
         this.event = event;
     }
-    
+
     /**
      * Builds the string to return the user
      * @param user the Steam user being queried
@@ -61,17 +63,17 @@ public class SteamUserQuery implements Runnable {
      */
     private String buildUserQueryString(SteamUser user) {
         StringBuilder builder = new StringBuilder();
-        
+
         builder.append(user.getPersonaName() + " is ");
         if(user.getPersonaState() == 0) builder.append(SteamUser.personaStates[user.getPersonaState()] + " (last online: " + IRCUtils.toReadableTime(new Date(user.getLastLogOff() * 1000), false, true) + " ago)");
         else if(user.getPersonaState() > 4) builder.append("online and is " + SteamUser.personaStates[user.getPersonaState()]);
         else builder.append(SteamUser.personaStates[user.getPersonaState()]);
         if(user.getGameExtraInfo() != null) builder.append(", currently in-game: " + user.getGameExtraInfo());
         if(user.getGameServerIp() != null) builder.append(" (server IP: " + user.getGameServerIp() + ")");
-        
+
         return builder.toString();
     }
-    
+
     /**
      * Checks to see whether the passed Steam ID is a numeric ID or community ID (string)
      * @param id the ID 
@@ -85,7 +87,7 @@ public class SteamUserQuery implements Runnable {
             return false;
         }
     }
-    
+
     /**
      * Method that executes upon thread start
      * (non-Javadoc)
@@ -94,15 +96,33 @@ public class SteamUserQuery implements Runnable {
     public void run() {
         SteamUser queriedUser;
         String givenId = event.getMessage().split(" ")[1];
-        try {
-            if(isNumericID(givenId)) {
-                queriedUser = new SteamUser(Long.parseLong(givenId), true);
-            } else {
-                queriedUser = new SteamUser(givenId, true);
+        if(!Configuration.getSteamAPIKey().isEmpty()) {
+            try {
+                if(isNumericID(givenId)) {
+                    queriedUser = new SteamUser(Long.parseLong(givenId), true);
+                } else {
+                    queriedUser = new SteamUser(givenId, true);
+                }
+                event.respond(buildUserQueryString(queriedUser));
+            } catch (IOException ex) {
+                if(ex.getMessage().equals("Server returned response code: 401")) {
+                    event.respond("Steam Web API key is invalid. Please add a valid Steam Web API key and reload configuration.");
+                    Configuration.getLogger().write(Level.WARNING, "Steam Web API key invalid");
+                }
+            } catch(JSONException ex) {
+                if(ex.getMessage().equals("JSONArray[0] not found.")) {
+                    event.respond("Could not retrieve Steam user details - user does not exist.");
+                } else {
+                    Configuration.getLogger().write(Level.WARNING, IRCUtils.getStackTraceString(ex));
+                }
+            } catch(NullPointerException ex) {
+                event.respond("Could not convert Community Name to 64-bit Steam ID. (User may not exist)");
+            } catch (Exception ex) {
+                Configuration.getLogger().write(Level.WARNING, IRCUtils.getStackTraceString(ex));
             }
-            event.respond(buildUserQueryString(queriedUser));
-        } catch (Exception ex) {
-            Configuration.getLogger().write(Level.WARNING, IRCUtils.getStackTraceString(ex));
+        } else {
+            event.respond("Steam Web API key has not been configured. Please add a valid Steam Web API key and reload configuration.");
+            Configuration.getLogger().write(Level.WARNING, "Steam Web API key not configured");
         }
     }
 }
